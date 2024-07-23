36720de328b6fc971e59bfc490e3b2257536c86a
sss <2740050471@qq.com>
2024-07-23 21:37:45 +0800

    adapter中加入流式传输

--- model_adapter/adapter/adapter.py
+++ model_adapter/adapter/adapter.py
@@ -21,11 +21,14 @@ class Adapter:
         self.server_sio.on('disconnect', self._on_disconnect)
         self.server_sio.on('registered', self._on_registered)
         self.server_sio.on('generate', self._on_generate)
+        self.server_sio.on('generate_stream', self._on_generate_stream)
 
         self.model_sio.on('connect', self._on_connect)
         self.model_sio.on('disconnect', self._on_disconnect)
         self.model_sio.on('register', self._on_register)
         self.model_sio.on('generate_finish', self._on_generate_finish)
+        self.model_sio.on('generate_streaming', self._on_generate_streaming)
+        self.model_sio.on('generate_stream_finish', self._on_generate_stream_finish)
 
     def _on_connect(self):
         print('Connection established')
@@ -49,6 +52,17 @@ class Adapter:
     def _on_generate_finish(self, data):
         print(f"generate finish: {data}")
         self.server_sio.emit('generate_finish', data=data)
+
+    def _on_generate_stream(self, data):
+        print(f"generate stream: {data}")
+        self.model_sio.emit('generate_stream', data=data)
+
+    def _on_generate_streaming(self, data):
+        self.server_sio.emit('generate_streaming', data=data)
+
+    def _on_generate_stream_finish(self, data):
+        print(f"generate stream finish: {data}")
+        self.server_sio.emit('generate_stream_finish', data=data)
 
     def run(self):
         while True:
--- model_adapter/model_server/test.py
+++ model_adapter/model_server/test.py
@@ -1,11 +1,15 @@
 from server import ModelProvider
+import time
 
 class Test(ModelProvider):
     def __init__(self):
         super().__init__('test', '0.0.0.0:7081')
 
-    def generate_fn(self, msg_list) -> str:
-        return 'test: ' + msg_list[-1]['content']
+    def generate_stream(self, msg_list):
+        result = 'test: ' + msg_list[-1]['content']
+        for i in result:
+            time.sleep(0.1)
+            yield i
 
 if __name__ == '__main__':
     test = Test()
