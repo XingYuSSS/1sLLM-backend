0ce2240a31e8d24cd7141d4ef5782c9c7883fbbb
ylhu <1565918412@qq.com>
2024-08-14 13:42:30 +0800

    显式声明参数

--- backend/main_fastapi.py
+++ backend/main_fastapi.py
@@ -77,51 +77,49 @@ class WebServer:
         """
         主页.
         """
-        return RedirectResponse(url='/static/index.html')
+        return RedirectResponse(url='/static/index.html', status_code=status.HTTP_302_FOUND)
 
-    async def user_exist(self, request: Request):
+    async def user_exist(self, user: str):
         """
         检查用户是否存在.
         """
-        username = base64.b64decode(request.query_params.get('user')).decode('utf-8')
+        username = base64.b64decode(user).decode('utf-8')
         if self.server.check_user_name_exist(username):
-            return JSONResponse(content='user_name_exist', status_code=200)
-        return JSONResponse(content='user_name_not_exist', status_code=200)
+            return JSONResponse(content='user_name_exist', status_code=status.HTTP_200_OK)
+        return JSONResponse(content='user_name_not_exist', status_code=status.HTTP_200_OK)
 
-    async def user_register(self, request: Request):
+    async def user_register(self, user: str, ic: str, sc: str, phone: str, pd: str):
         """
         注册.
         """
-        params = request.query_params
-        username = base64.b64decode(params.get('user')).decode('utf-8')
+        username = base64.b64decode(user).decode('utf-8')
         if self.server.check_user_name_exist(username):
-            return JSONResponse(content='user_name_exist', status_code=200)
-        invite_code = base64.b64decode(params.get('ic')).decode('utf-8')
+            return JSONResponse(content='user_name_exist', status_code=status.HTTP_200_OK)
+        invite_code = base64.b64decode(ic).decode('utf-8')
         validation = self.invite_code_manager.validate_code(invite_code)
         if validation == 0:
-            return JSONResponse(content='invalid_invite_code', status_code=200)
+            return JSONResponse(content='invalid_invite_code', status_code=status.HTTP_200_OK)
         elif validation == -1:
-            return JSONResponse(content='used_invite_code', status_code=200)
+            return JSONResponse(content='used_invite_code', status_code=status.HTTP_200_OK)
         else:
             self.invite_code_manager.mark_code_as_used(invite_code)
-        sms_code = base64.b64decode(params.get('sc')).decode('utf-8')
-        phone = base64.b64decode(params.get('phone')).decode('utf-8')
+        sms_code = base64.b64decode(sc).decode('utf-8')
+        phone = base64.b64decode(phone).decode('utf-8')
         if sms_code != self.tmp_sms_code.get(phone):
-            return JSONResponse(content='invalid_sms_code', status_code=200)
+            return JSONResponse(content='invalid_sms_code', status_code=status.HTTP_200_OK)
         del self.tmp_sms_code[phone]
-        password = base64.b64decode(params.get('pd')).decode('utf-8')
+        password = base64.b64decode(pd).decode('utf-8')
         password_md5 = hashlib.md5(password.encode('utf-8')).hexdigest()
         user = data.User(username, password_md5, phone)
         self.server.add_user(user)
-        return JSONResponse(content='success', status_code=200)
+        return JSONResponse(content='success', status_code=status.HTTP_200_OK)
 
-    async def user_login(self, request: Request):
+    async def user_login(self, request: Request, user: str, pd: str):
         """
         登录.
         """
-        params = request.query_params
-        username = base64.b64decode(params.get('user')).decode('utf-8')
-        password = base64.b64decode(params.get('pd')).decode('utf-8')
+        username = base64.b64decode(user).decode('utf-8')
+        password = base64.b64decode(pd).decode('utf-8')
         if username is None or password is None:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="invalid_username_or_password")
         password_md5 = hashlib.md5(password.encode('utf-8')).hexdigest()
@@ -132,7 +130,7 @@ class WebServer:
         sid = self.server.gen_session_id(username)
         self.server.set_session_dict(username, sid)
         request.session.update({'username': username, 'session_id': sid})
-        return JSONResponse(content={'success': True, 'session_id': sid}, status_code=200)
+        return JSONResponse(content={'success': True, 'session_id': sid}, status_code=status.HTTP_200_OK)
 
     async def user_logout(self, request: Request):
         """
@@ -143,15 +141,15 @@ class WebServer:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid_username")
         request.session.clear()
         self.server.pop_session_dict(username)
-        return JSONResponse(content='success', status_code=200)
+        return JSONResponse(content='success', status_code=status.HTTP_200_OK)
 
-    async def sms_code_get(self, request: Request):
+    async def sms_code_get(self, phone: str):
         """
         获取短信验证码.
         """
-        phone = base64.b64decode(request.query_params.get('phone')).decode('utf-8')
+        phone = base64.b64decode(phone).decode('utf-8')
         if not re.match(r'^1[3456789]\d{9}$', phone):
-            return JSONResponse(content='invalid_phone', status_code=200)
+            return JSONResponse(content='invalid_phone', status_code=status.HTTP_200_OK)
         sms_code = str(random.randint(100000, 999999))
         # TODO: 发送短信
         while True:
@@ -182,13 +180,13 @@ class WebServer:
                 time.sleep(1)
 
         self.tmp_sms_code[phone] = sms_code
-        return JSONResponse(content='success', status_code=200)
+        return JSONResponse(content='success', status_code=status.HTTP_200_OK)
 
     async def api_providers(self):
         """
         获取全局支持的服务商.
         """
-        return JSONResponse(content=Api.get_providers(), status_code=200)
+        return JSONResponse(content=Api.get_providers(), status_code=status.HTTP_200_OK)
 
     async def api_models(self, request: Request):
         """
@@ -199,7 +197,7 @@ class WebServer:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid_user")
         user.refresh_model('Scir')
         models = user.get_available_models()
-        return JSONResponse(content=models, status_code=200)
+        return JSONResponse(content=models, status_code=status.HTTP_200_OK)
 
     async def api_list(self, request: Request):
         """
@@ -208,35 +206,33 @@ class WebServer:
         user = self.server.get_user(request.session.get('username'), request.session.get('session_id'))
         if user is None:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid_user")
-        return JSONResponse(content=user.get_api_list(), status_code=200)
+        return JSONResponse(content=user.get_api_list(), status_code=status.HTTP_200_OK)
 
-    async def api_add(self, request: Request):
+    async def api_add(self, request: Request, name: str, key: str):
         """
         添加api.
         """
-        params = request.query_params
-        service_provider_name = base64.b64decode(params.get('name')).decode('utf-8')
-        api_key = base64.b64decode(params.get('key')).decode('utf-8')
+        service_provider_name = base64.b64decode(name).decode('utf-8')
+        api_key = base64.b64decode(key).decode('utf-8')
         user = self.server.get_user(request.session.get('username'), request.session.get('session_id'))
         if user is None:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid_user")
         if not user.add_api(service_provider_name, api_key):
-            return JSONResponse(content='invalid_service_provider_name_or_key', status_code=200)
-        return JSONResponse(content='success', status_code=200)
+            return JSONResponse(content='invalid_service_provider_name_or_key', status_code=status.HTTP_200_OK)
+        return JSONResponse(content='success', status_code=status.HTTP_200_OK)
 
-    async def api_del(self, request: Request):
+    async def api_del(self, request: Request, name: str):
         """
         删除api.
         """
-        params = request.query_params
         user = self.server.get_user(request.session.get('username'), request.session.get('session_id'))
         if user is None:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid_user")
-        service_provider_name = params.get('name')
+        service_provider_name = name
         if service_provider_name is None:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid_service_provider_name")
         user.del_api(service_provider_name)
-        return JSONResponse(content='success', status_code=200)
+        return JSONResponse(content='success', status_code=status.HTTP_200_OK)
 
     async def chat_list(self, request: Request):
         """
@@ -246,80 +242,74 @@ class WebServer:
         if user is None:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid_user")
         response = {k: v._to_db_dict(v) for k,v in user.get_chat_dict().items()}
-        return JSONResponse(content=response, status_code=200)
+        return JSONResponse(content=response, status_code=status.HTTP_200_OK)
 
-    async def chat_new(self, request: Request):
+    async def chat_new(self, request: Request, title: str):
         """
         新建聊天记录.
         """
-        params = request.query_params
         user = self.server.get_user(request.session.get('username'), request.session.get('session_id'))
         if user is None:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid_user")
         cid = self.server.gen_chat_id(user.get_username())
-        chat = data.Chat(cid, base64.b64decode(params.get('title')).decode('utf-8'))
+        chat = data.Chat(cid, base64.b64decode(title).decode('utf-8'))
         user.add_chat(chat)
-        return JSONResponse(cid, status_code=200)
+        return JSONResponse(cid, status_code=status.HTTP_200_OK)
 
-    async def chat_get(self, request: Request):
+    async def chat_get(self, request: Request, cid: str):
         """
         获取聊天记录.
         """
-        params = request.query_params
         user = self.server.get_user(request.session.get('username'), request.session.get('session_id'))
         if user is None:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid_user")
-        cid = params.get('cid')
         chat = self.server.get_chat(user, base64.b64decode(cid).decode('utf-8'))
         if chat is None:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid_chat_id")
         response = chat._to_db_dict(chat)
-        return JSONResponse(content=response, status_code=200)
+        return JSONResponse(content=response, status_code=status.HTTP_200_OK)
 
-    async def chat_title(self, request: Request):
+    async def chat_title(self, request: Request, cid: str, title: str):
         """
         更新聊天记录标题.
         """
-        params = request.query_params
         user = self.server.get_user(request.session.get('username'), request.session.get('session_id'))
         if user is None:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid_user")
-        chat = self.server.get_chat(user, base64.b64decode(params.get('cid')).decode('utf-8'))
+        chat = self.server.get_chat(user, base64.b64decode(cid).decode('utf-8'))
         if chat is None:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid_chat_id")
-        title = base64.b64decode(params.get('title')).decode('utf-8')
+        title = base64.b64decode(title).decode('utf-8')
         chat.set_title(title)
-        return JSONResponse(content='success', status_code=200)
+        return JSONResponse(content='success', status_code=status.HTTP_200_OK)
 
-    async def chat_del(self, request: Request):
+    async def chat_del(self, request: Request, cid: str):
         """
         删除聊天记录.
         """
-        params = request.query_params
         user = self.server.get_user(request.session.get('username'), request.session.get('session_id'))
         if user is None:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid_user")
-        chat = self.server.get_chat(user, base64.b64decode(params.get('cid')).decode('utf-8'))
+        chat = self.server.get_chat(user, base64.b64decode(cid).decode('utf-8'))
         if chat is None:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid_chat_id")
         user.del_chat(chat.get_chat_id())
-        return JSONResponse(content='success', status_code=200)
+        return JSONResponse(content='success', status_code=status.HTTP_200_OK)
 
-    async def chat_gen(self, request: Request):
+    async def chat_gen(self, request: Request, cid: str, p: str, provider_models: str):
         """
         生成聊天回复.
         """
-        params = request.query_params
         user = self.server.get_user(request.session.get('username'), request.session.get('session_id'))
         if user is None:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid_user")
-        chat = self.server.get_chat(user, base64.b64decode(params.get('cid')).decode('utf-8'))
+        chat = self.server.get_chat(user, base64.b64decode(cid).decode('utf-8'))
         if chat is None:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid_chat_id")
-        prompt = base64.b64decode(params.get('p')).decode('utf-8')
+        prompt = base64.b64decode(p).decode('utf-8')
         if prompt is None:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid_prompt")
-        provider_models = json.loads(base64.b64decode(params.get('provider_models')).decode('utf-8'))
+        provider_models = json.loads(base64.b64decode(provider_models).decode('utf-8'))
         send_msg = data.Message('user', user.get_username(), prompt)
         chat.add_msg(send_msg)
         responses = Api.get_responses(chat, provider_models, user.get_api_dict())
@@ -332,23 +322,23 @@ class WebServer:
             chat.sel_recv_msg(responses[0]['model'])
         user.add_chat(chat)
         response = {k: v.__dict__() for k, v in recv_msg_list.items()}
-        return JSONResponse(content=response, status_code=200)
+        return JSONResponse(content=response, status_code=status.HTTP_200_OK)
 
-    async def chat_gen_stream(self, request: Request):
+    async def chat_gen_stream(self, request: Request, cid: str, p: str, provider_models: str):
         """
         流式生成聊天回复.
         """
-        params = request.query_params
+        
         user = self.server.get_user(request.session.get('username'), request.session.get('session_id'))
         if user is None:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid_user")
-        chat = self.server.get_chat(user, base64.b64decode(params.get('cid')).decode('utf-8'))
+        chat = self.server.get_chat(user, base64.b64decode(cid).decode('utf-8'))
         if chat is None:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid_chat_id")
-        prompt = base64.b64decode(params.get('p')).decode('utf-8')
+        prompt = base64.b64decode(p).decode('utf-8')
         if prompt is None:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid_prompt")
-        provider_models = json.loads(base64.b64decode(params.get('provider_models')).decode('utf-8'))
+        provider_models = json.loads(base64.b64decode(provider_models).decode('utf-8'))
         send_msg = data.Message('user', user.get_username(), prompt)
         chat.add_msg(send_msg)
         stream = Api.get_responses_stream(chat, provider_models, user.get_api_dict())
@@ -377,22 +367,22 @@ class WebServer:
                 chat.sel_recv_msg(list(responses.keys())[0])
         return StreamingResponse(res(), 200)
     
-    async def chat_sel(self, request: Request):
+    async def chat_sel(self, request: Request, cid: str, name: str):
         """
         选择聊天.
         """
-        params = request.query_params
+        
         user = self.server.get_user(request.session.get('username'), request.session.get('session_id'))
         if user is None:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid_user")
-        chat = self.server.get_chat(user, base64.b64decode(params.get('cid')).decode('utf-8'))
+        chat = self.server.get_chat(user, base64.b64decode(cid).decode('utf-8'))
         if chat is None:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid_chat_id")
-        model_name = base64.b64decode(params.get('name')).decode('utf-8')
+        model_name = base64.b64decode(name).decode('utf-8')
         if model_name not in chat.get_recv_msg_tmp():
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid_model_name")
         chat.sel_recv_msg(model_name)
-        return JSONResponse(content='success', status_code=200)
+        return JSONResponse(content='success', status_code=status.HTTP_200_OK)
 
 
 if __name__ == "__main__":
